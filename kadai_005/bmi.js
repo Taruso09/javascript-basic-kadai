ea6e6054dae58b2dee34ac367bae31bb51b748a1
Taruso09 <r0i9o2a2@softbank.ne.jp>
2025-03-25 08:26:24 +0900

    修正4

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,23 +1,19 @@
-let weight;
-weight = '体重';
-let kg = 68 ;
+let weight ;
+weight = '体重'
+let kg = 68;
 
 console.log(weight);
 console.log(kg);
 
 let height;
-height ='身長';
-let m = 1.7;
+height ='身長'
+let m =1.7;
 console.log(height);
 console.log(m);
 
-
+let BMI;
+BMI = 'BMI(体格指数)';
 //四則演算
+console.log(BMI);
 console.log(kg/m*m);
 
-let BMI ;
-BMI ='BMI(体格指数)';
-let total = 68;
-
-console.log(BMI);
-console.log(total);
