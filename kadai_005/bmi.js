e2275819bd47455052b065a33d46a440187a00e7
Taruso09 <r0i9o2a2@softbank.ne.jp>
2025-03-24 22:18:35 +0900

    課題提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+let weight;
+
+weight ='体重';
+
+let kg = 68;
+
+console.log(weight);
+console.log(kg);
+
+let heigt;
+
+heigt ='身長';
+
+let m = 1.7;
+
+console.log(heigt);
+console.log(m);
+
+//四則演算
+console.log(68-1.7*1.7);
