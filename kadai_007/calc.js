67a1abd31d1b57875bf0052f6406019c2cd0bf05
Taruso09 <r0i9o2a2@softbank.ne.jp>
2025-03-27 23:03:44 +0900

    課題を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+num = Math.floor(Math.random()*16);
+
+console.log(num);
+
+switch(true){
+  case (num % 3 == 0):
+    console.log('3の倍数です');
+    break;
+  case (num % 5 == 0):
+    console.log('5の倍数です');
+    break;
+  case(num % 3 == 0 && num % 5 == 0):
+    console.log('3と5の倍数です');
+    break;
+  default:
+    console.log(num);
+    break;
+}
